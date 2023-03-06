04bdeab37bbb04e0f93e938754f071283d4a52c0
minhnh <minhnh.da@gmail.com>
2023-03-06 21:14:30 +0900

    init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,10 @@
 import '@/styles/globals.css'
+/* Additional styles */
+import '@/styles/additional-styles/utility-patterns.css';
+import '@/styles/additional-styles/custom-fonts.css';
+import '@/styles/additional-styles/range-slider.css';
+import '@/styles/additional-styles/toggle-switch.css';
+import '@/styles/additional-styles/theme.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
