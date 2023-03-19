08a1db69bfe9d2ef1362df9b6f322ccad52c702d
minhnh <minhnh.da@gmail.com>
2023-03-19 23:56:25 +0900

    fix sidenav highlight not working correctly

--- src/partials/SideNavigation.jsx
+++ src/partials/SideNavigation.jsx
@@ -24,11 +24,10 @@ function SideNavigation() {
           <nav className="w-full">
             <ul className="space-y-4">
               <li className="py-2">
-                {/* TODO: fix here or highlight doesn't work when viewing a blogpost */}
                 <Link
                   href="/"
                   className={ `w-full h-6 flex items-center justify-center relative after:absolute after:w-0.5 after:right-0 after:top-0 after:bottom-0 ${
-                    pathname === '/' || pathname === '/post'
+                    pathname === '/' || pathname.includes('en')
                       ? 'text-sky-500 after:bg-sky-500'
                       : 'text-slate-400 hover:text-slate-500 dark:text-slate-500 dark:hover:text-slate-400'
                   }` }
