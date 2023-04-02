3e5c6f82ef8d41c56438c3f3b8dfd7aabd133f99
minhnh <minhnh.da@gmail.com>
2023-04-02 12:51:15 +0900

    share buttons inside articles now work properly

--- src/pages/en/[tag]/[post].jsx
+++ src/pages/en/[tag]/[post].jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from "next/link"
+import { useRouter } from "next/router"
+
 import SideNavigation from '../../../partials/SideNavigation'
 import Header from '../../../partials/Header'
 import WidgetNewsletter from '../../../partials/WidgetNewsletter'
@@ -17,6 +19,10 @@ import { getPageContentAsMarkdownById, getPageIdBySlug, getPagePropertiesById, g
 import { CustomLink } from "@/partials/CustomLink"
 
 function Post({ content, pageProperties, pagesForSidebar, readingStats }) {
+  const router = useRouter()
+  const currentPageURL = 'https://tsukie.com' + router.asPath
+
+
   return (
     <div className="max-w-7xl mx-auto">
       <div className="min-h-screen flex">
@@ -58,10 +64,12 @@ function Post({ content, pageProperties, pagesForSidebar, readingStats }) {
                           <li>
                             <a
                               className="flex justify-center items-center text-slate-400 dark:text-slate-500 hover:text-sky-500 dark:hover:text-sky-500 transition duration-150 ease-in-out"
-                              href="src/pages#0"
+                              href={ `https://twitter.com/intent/tweet?url=${ currentPageURL }` }
                               aria-label="Twitter"
+                              target="_blank"
+                              rel="nofollow noopener noreferrer"
                             >
-                              <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                              <svg className=" w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                   d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z"/>
                               </svg>
@@ -70,23 +78,13 @@ function Post({ content, pageProperties, pagesForSidebar, readingStats }) {
                           <li>
                             <a
                               className="flex justify-center items-center text-slate-400 dark:text-slate-500 hover:text-sky-500 dark:hover:text-sky-500 transition duration-150 ease-in-out"
-                              href="src/pages#0"
+                              href={ `https://www.facebook.com/sharer/sharer.php?u=${ currentPageURL }` }
                               aria-label="Facebook"
+                              target="_blank"
+                              rel="nofollow noopener noreferrer"
                             >
                               <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M14.023 24 14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257Z"/>
-                              </svg>
-                            </a>
-                          </li>
-                          <li>
-                            <a
-                              className="flex justify-center items-center text-slate-400 dark:text-slate-500 hover:text-sky-500 dark:hover:text-sky-500 transition duration-150 ease-in-out"
-                              href="src/pages#0"
-                              aria-label="Share"
-                            >
-                              <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                  d="M20 14c1.654 0 3-1.346 3-3s-1.346-3-3-3-3 1.346-3 3c0 .223.029.439.075.649l-3.22 2.012A2.97 2.97 0 0 0 12 13c-1.654 0-3 1.346-3 3s1.346 3 3 3a2.97 2.97 0 0 0 1.855-.661l3.22 2.012c-.046.21-.075.426-.075.649 0 1.654 1.346 3 3 3s3-1.346 3-3-1.346-3-3-3a2.97 2.97 0 0 0-1.855.661l-3.22-2.012c.046-.21.075-.426.075-.649 0-.223-.029-.439-.075-.649l3.22-2.012A2.97 2.97 0 0 0 20 14Z"/>
                               </svg>
                             </a>
                           </li>
