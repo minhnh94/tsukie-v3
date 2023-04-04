1c7f56c370ebd15e381fc8d54a8adcc7bf97c8a7
minhnh <minhnh.da@gmail.com>
2023-04-04 23:39:10 +0900

    (temporary) wallcal acknowledgements page

--- src/partials/SideNavigation.jsx
+++ src/partials/SideNavigation.jsx
@@ -26,7 +26,7 @@ function SideNavigation() {
                 <Link
                   href="/"
                   className={ `w-full h-6 flex items-center justify-center relative after:absolute after:w-0.5 after:right-0 after:top-0 after:bottom-0 ${
-                    pathname === '/' || pathname.includes('en')
+                    pathname === '/' || pathname.includes('/en/')
                       ? 'text-sky-500 after:bg-sky-500'
                       : 'text-slate-400 hover:text-slate-500 dark:text-slate-500 dark:hover:text-slate-400'
                   }` }
