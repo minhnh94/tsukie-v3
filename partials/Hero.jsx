1c6d6cec46f0012744d636bcbb83843b7ebb7947
minhnh <minhnh.da@gmail.com>
2024-09-08 22:17:55 +0900

    change site hero to be more punchy

--- partials/Hero.jsx
+++ partials/Hero.jsx
@@ -8,10 +8,11 @@ function Hero() {
         <div className="pt-8 pb-10">
           <img className="rounded-full mb-5" src="/images/logo.png" width="56" height="56" alt="Me"/>
           <h1 className="h1 font-aspekta mb-5">
-            <Highlight>Coding</Highlight> Capers, <Highlight>Life</Highlight> Lessons & the <Highlight>Indie Hacker</Highlight> Escapade
+            <Highlight>Tech</Highlight> Whiz, <Highlight>Soul</Highlight> Searcher, <Highlight>Dream</Highlight> Chaser
           </h1>
           <p className="text-lg text-slate-500 dark:text-slate-400">
-            Welcome to <span className="font-medium text-sky-500">tsukie.com</span>! Explore coding, life lessons, and my indie hacker journey as I share my experiences in technology, personal growth, and entrepreneurship.
+            Welcome to <span className="font-medium text-sky-500">tsukie.com</span>, my cozy digital home on the web! I write about tech, personal thoughts and lessons, and my attempts to make a dent in the universe.<br/>
+            100% human written content, completely free of AI/LLM regurgitated nonsense 😉
           </p>
         </div>
       </div>
