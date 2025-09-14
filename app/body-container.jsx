11ea1eca3e048e9dc745c2039b247cedac795ee2
minhnh <minhnh.da@gmail.com>
2025-09-14 14:59:40 +0900

    change plausible integration, npm audit

--- app/body-container.jsx
+++ app/body-container.jsx
@@ -1,12 +1,22 @@
-'use client'
-import { Analytics } from "@vercel/analytics/react"
-import { ThemeProvider } from "next-themes"
+"use client";
+import { Analytics } from "@vercel/analytics/react";
+import PlausibleProvider from "next-plausible";
+import { ThemeProvider } from "next-themes";
 
 export default function BodyContainer({ children }) {
   return (
-    <ThemeProvider defaultTheme="dark" attribute="class">
-      { children }
-      <Analytics/>
-    </ThemeProvider>
-  )
+    <PlausibleProvider
+      domain="tsukie.com"
+      scriptProps={{ defer: true }}
+      trackOutboundLinks={true}
+      taggedEvents={true}
+      customDomain="https://plausible.tsukie.com"
+      selfHosted={true}
+    >
+      <ThemeProvider defaultTheme="dark" attribute="class">
+        {children}
+        <Analytics />
+      </ThemeProvider>
+    </PlausibleProvider>
+  );
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -4,17 +4,6 @@ import BodyContainer from "@/app/body-container";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <head>
-        <script
-          defer
-          data-domain="tsukie.com"
-          src="https://plausible.tsukie.com/js/script.outbound-links.pageview-props.tagged-events.js"
-        ></script>
-        <script>
-          window.plausible = window.plausible || function(){" "}
-          {(window.plausible.q = window.plausible.q || []).push(arguments)}
-        </script>
-      </head>
       <body className="font-inter antialiased bg-white text-slate-800 dark:bg-slate-900 dark:text-slate-200 tracking-tight">
         <BodyContainer>{children}</BodyContainer>
       </body>
