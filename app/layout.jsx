d47d3c88079472b4b29df2ba52fad3b1e930c0cb
minhnh <minhnh.da@gmail.com>
2025-09-14 14:46:11 +0900

    add plausible data collection

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,23 @@
-import '@/styles/globals.css'
-import BodyContainer from "@/app/body-container"
+import "@/styles/globals.css";
+import BodyContainer from "@/app/body-container";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-    <body className="font-inter antialiased bg-white text-slate-800 dark:bg-slate-900 dark:text-slate-200 tracking-tight">
-    <BodyContainer>
-      { children }
-    </BodyContainer>
-    </body>
+      <head>
+        <script
+          defer
+          data-domain="tsukie.com"
+          src="https://plausible.tsukie.com/js/script.outbound-links.pageview-props.tagged-events.js"
+        ></script>
+        <script>
+          window.plausible = window.plausible || function(){" "}
+          {(window.plausible.q = window.plausible.q || []).push(arguments)}
+        </script>
+      </head>
+      <body className="font-inter antialiased bg-white text-slate-800 dark:bg-slate-900 dark:text-slate-200 tracking-tight">
+        <BodyContainer>{children}</BodyContainer>
+      </body>
     </html>
-  )
+  );
 }
