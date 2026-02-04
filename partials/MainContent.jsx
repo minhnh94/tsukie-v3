9a9a19cd60418d3902842bd6112993ef3d15ad33
minhnh <minhnh.da@gmail.com>
2026-02-04 22:54:59 +0900

    fix long link string breaking responsive

--- partials/MainContent.jsx
+++ partials/MainContent.jsx
@@ -4,7 +4,7 @@ import Footer from "@/partials/Footer"
 
 function MainContent(props) {
   return (
-    <main className="grow min-w-0 px-6">
+    <main className="grow min-w-0 overflow-hidden supports-[overflow:clip]:overflow-clip px-6">
       <div className="w-full h-full max-w-[1072px] mx-auto flex flex-col">
         <Header />
         {props.children}
